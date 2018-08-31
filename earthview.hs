8ed5aed16b26d73762995a752816b9c364dac176
PixeL <officialpixelinc@gmail.com>
2018-08-31 07:54:58 -0500

    Fix up some things

--- earthview.hs
+++ earthview.hs
@@ -1,31 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString as B
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
 import Network.HTTP
 import Network.URI (parseURI)
 import Control.Monad
-import Text.Printf
--- I don't think this program even compiles at the moment.
+
+packString :: String -> B.ByteString
+packString = encodeUtf8 . T.pack
 
 downloadImage :: String -> String -> IO ()
 downloadImage url file = do
     request <- get url
-    case request of
-        Nothing -> putStrLn $ "Invalid status code!"
-        Just image -> B.writeFile file image
-  where
-    get url = let uri = case parseURI url of
-                      Nothing -> error $ "Invalid URI: " ++ url
-                      Just u  -> u in simpleHTTP (defaultGETRequest_ uri) >>= getResponseBody
+    B.writeFile file (packString request)
 
--- This doesn't work currently.
-get :: String -> IO (Maybe (IO String))
+-- TODO: Check status code and do something if it's not 200
+get :: String -> IO String
 get url = do
     request <- simpleHTTP (getRequest url)
-    responseCode <- getResponseCode request
-    if responseCode /= (2, 0, 0) then
-        return Nothing
-    else
-       return (Just (getResponseBody request))
-
-main :: IO ()
-main =  mapM_ print [1..10]
+    return request >>= getResponseBody
